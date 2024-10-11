1a8312ae496bac907a34ad2f26156490b3ff8d81
alfaysal <all.faysal@pathao.com>
2024-10-11 21:48:51 +0600

    Add api controller & scaffolding.

---
+++ app/Http/Controllers/V1/ApiController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+
+abstract class ApiController extends Controller
+{
+    public function respond($message, $type = 'success', $code = 200, $data = []): JsonResponse
+    {
+        $response = [
+            'message' => $message,
+            'type' => $type,
+            'code' => $code,
+        ];
+
+        if ((bool) $data) {
+            $response['data'] = $data;
+        }
+
+        return response()->json($response, $code);
+    }
+
+    protected function success(array $data = [], int $code = 200, string $message = ''): JsonResponse
+    {
+        return $this->respond($message, 'success', $code, $data);
+    }
+
+    public function error($code = 422, $data = [], $message = 'Something went wrong! Please try again.'): JsonResponse
+    {
+        $response = [
+            'message' => $message,
+            'type' => 'error',
+            'code' => $code,
+        ];
+
+        if ((bool) $data) {
+            $response['errors'] = $data;
+        }
+
+        return response()->json($response, $code);
+    }
+}
