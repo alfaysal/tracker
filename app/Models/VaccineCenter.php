5013b33b3c18ea8ccf0907908a034ea89fe20d57
alfaysal <all.faysal@pathao.com>
2024-10-12 11:50:26 +0600

    add seeder & factory for user's & vaccine center.

--- app/Models/VaccineCenter.php
+++ app/Models/VaccineCenter.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class VaccineCenter extends Model
 {
     use HasFactory;
+
+    protected $table = 'vaccine_centers';
+    protected $fillable = [
+        'name',
+        'user_limit_per_day'
+    ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
