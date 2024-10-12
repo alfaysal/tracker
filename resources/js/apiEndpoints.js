a537a87e7e818446c6dac8bfc23b9de3154d446a
alfaysal <all.faysal@pathao.com>
2024-10-13 01:58:05 +0600

    add registration & search form.

--- resources/js/apiEndpoints.js
+++ resources/js/apiEndpoints.js
@@ -1,3 +1,4 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const GET_VACCINE_CENTERS_URL = `${API_URL}/vaccine-centers`
+export const GET_VACCINE_CENTERS_URL = `${API_URL}/vaccine-centers`;
+export const GET_USER_STATUS_SEARCH_URL = `${API_URL}/user/searches`;
---
+++ resources/js/components/SearchBar.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <form class="max-w-md mx-auto" @submit.prevent="serachByNId">
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="text-red-800 text-center mb-2" v-if="failedReason">
+                <h4>{{ searchValidationMessages[failedReason] }}</h4>
+            </div>
+            <div class="relative">
+                <input
+                    v-model="nid"
+                    type="search"
+                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Search by nid"
+                    required
+                />
+                <button type="submit"
+                        class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">
+                    Search
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import {GET_USER_STATUS_SEARCH_URL} from "../apiEndpoints.js";
+
+import {ref} from "vue";
+const nid = ref('');
+const nidIsEmpty = ref(false);
+const failedReason = ref(null);
+const searchValidationMessages = ref({
+    empty: "Nid can not empty!!!",
+    length: "length must be 10 chars"
+});
+const serachByNId = () => {
+    nidIsEmpty.value = false;
+    failedReason.value = null;
+
+    let trimedNid = nid.value.trim();
+
+    if (!trimedNid.length) {
+        failedReason.value = 'empty';
+        nidIsEmpty.value = true;
+        return;
+    }
+
+    if (trimedNid.length !== 10) {
+        failedReason.value = 'length';
+        return;
+    }
+
+    axios.post(GET_USER_STATUS_SEARCH_URL, {nid: nid.value}).then((response) => {
+        nid.value = '';
+        console.log(response)
+    });
+}
+</script>
+
+
+<style scoped>
+
+</style>
